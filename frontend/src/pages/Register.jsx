054946b29e18a95d5286342536b5bcce784933d4
Alison Butcher <alison@alisonbutcher.com>
2022-08-21 18:53:29 +1000

    Added support for private routes to tickets

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
-import {toast} from 'react-toastify';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Register() {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
         password: "",
-        password2: "",
+        password2: "", 
     });
 
     const { name, email, password, password2 } = formData;
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        // Redirect when logged in
+        if (isSuccess || user) {
+            navigate("/");
+        }
+
+        dispatch(reset)
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -22,11 +46,23 @@ function Register() {
     const onSubmit = (e) => {
         e.preventDefault();
 
-        if(password !== password2) {
-            toast.error('Passwords do not match')
-        }
+        if (password !== password2) {
+            toast.error("Passwords do not match");
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            };
 
+            dispatch(register(userData));
+        }
+    };
+
+    if(isLoading) {
+        return <Spinner />
     }
+
     return (
         <>
             <section className="heading">
